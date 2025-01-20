e6ccaeaacbe3899fe56930f8444116f86587c003
_w3ndell <academy.wendell@gmail.com>
2025-01-20 18:03:26 -0300

    Somando tentativas e exibindo informações

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let numeroSecreto = gerarNumero();
+let tentativas = 1;
 
 function exibirTexto(tag, texto) {
   let campo = document.querySelector(tag);
@@ -11,8 +12,24 @@ function gerarNumero() {
 
 function verificarChute() {
   let chute = document.querySelector("input").value;
-  console.log(chute == numeroSecreto);
+  let mensagem = tentativas > 1 ? "Tentativas" : "Tentativa";
+  if (chute == numeroSecreto) {
+    exibirTexto("h1", "Acertou!");
+    exibirTexto(
+      "p",
+      `Voce descobriu o numero secreto, com ${tentativas} ${mensagem}`
+    );
+  } else {
+    if (chute > numeroSecreto) {
+      exibirTexto("p", "O numero secreto e Menor!");
+    } else {
+      exibirTexto("p", "O numero secreto e Maior!");
+    }
+  }
+  tentativas++;
 }
+
+function reinciarJogo() {}
 
 exibirTexto("h1", "Jogo do numero secreto");
 exibirTexto("p", "Escolha o numero entre 1 e 10");
