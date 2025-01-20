5ea7102b24aef67f067aead5b61fb0aac8b73d92
_w3ndell <academy.wendell@gmail.com>
2025-01-20 17:29:40 -0300

    utilizando dom

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-function exibirTexto(tag, texto){
-    let campo = document.querySelector(tag)
-    campo.innerHTML = texto
+let numeroSecreto = gerarNumero();
+
+function exibirTexto(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
 }
 
-exibirTexto("h1", "Jogo do numero secreto")
-exibirTexto("p", "Escolha o numero entre 1 e 10")
-
-function verificarChute(){
-    
+function gerarNumero() {
+  return parseInt(Math.random() * 10 + 1);
 }
+
+function verificarChute() {
+  let chute = document.querySelector("input").value;
+  console.log(chute == numeroSecreto);
+}
+
+exibirTexto("h1", "Jogo do numero secreto");
+exibirTexto("p", "Escolha o numero entre 1 e 10");
